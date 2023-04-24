b8f674528886471bad0d9abc3672b164f9e46671
janne <ennaj_janne@hotmail.com>
2023-04-24 14:30:32 +0200

    - variables made clearer - the input no longer needs to be in uppercase

--- src/playersguide/day20/ComposedSoup.java
+++ src/playersguide/day20/ComposedSoup.java
@@ -1,18 +1,19 @@
 package playersguide.day20;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ComposedSoup {
     static void ComposedSoup() {
 
         Scanner inputKeyboard = new Scanner(System.in);
-        System.out.println("Chose what kind of recipe you want, you can chose out: SOUP, STEW and GUMBO");
-        TypeRecipe one = TypeRecipe.valueOf(inputKeyboard.nextLine());
-        System.out.println("Chose what kind of main ingredient you want, you can chose out:  POTATO, MUSHROOM,CARROT and CHICKEN");
-        MainIngredient two = MainIngredient.valueOf(inputKeyboard.nextLine());
-        System.out.println("Chose what kind of seasoning you want, you can chose out:  SPICY, SWEET and SALTY");
-        Seasoning three = Seasoning.valueOf(inputKeyboard.nextLine());
-        System.out.println("You get: " + three.nameSeasoning + two.nameIngredient + one.nameTypeRecipe);
+        System.out.println("Chose what kind of recipe you want, you can chose out: soup, stew and gumbo");
+        TypeRecipe typeRecipe = TypeRecipe.valueOf(inputKeyboard.nextLine().toUpperCase(Locale.ROOT));
+        System.out.println("Chose what kind of main ingredient you want, you can chose out:  potato, mushroom,carrot and chicken");
+        MainIngredient mainIngredient = MainIngredient.valueOf(inputKeyboard.nextLine().toUpperCase(Locale.ROOT));
+        System.out.println("Chose what kind of seasoning you want, you can chose out: spicy, sweet and salty");
+        Seasoning seasoning = Seasoning.valueOf(inputKeyboard.nextLine().toUpperCase(Locale.ROOT));
+        System.out.println("You get: " + seasoning.nameSeasoning + mainIngredient.nameIngredient + typeRecipe.nameTypeRecipe);
 
 
     }

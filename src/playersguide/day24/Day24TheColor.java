78a429660fa434152844b829adc8a8a467adac9e
janne <ennaj_janne@hotmail.com>
2023-05-25 13:55:08 +0200

    new Color class with fields for its red, green, and blue channels

--- src/playersguide/day24/Day24TheColor.java
+++ src/playersguide/day24/Day24TheColor.java
@@ -22,12 +22,32 @@ package playersguide.day24;
 //GeeksForGeeks
 public class Day24TheColor {
     public static void main(String[] args) {
+    Color pink = new Color(255,192,203);
+    Color red = Color.red;
 
     }
-    public class Color{
 
-      int[] red = {255, 0, 0};
-      int []green= {0, 128, 0};
-      int [] blue= {0, 0, 255};
+    public static class Color{
+
+       int redValue = 255;
+       int greenValue= 128;
+       int blueValue= 255;
+
+        final static Color white = new Color(255,255, 255);
+        final static Color black = new Color(0,0, 0);
+        final static Color red = new Color(255,0, 0);
+        final static Color orange = new Color(255,165, 0);
+        final static Color yellow = new Color(255,255, 0);
+        final static Color green = new Color(0,128, 0);
+        final static Color blue = new Color(0,0, 255);
+        final static Color purple = new Color(128,0, 128);
+
+
+         public Color(int redValue, int greenValue, int blueValue) {
+            this.redValue = redValue;
+            this.greenValue = greenValue;
+            this.blueValue = blueValue;
+        }
     }
-}
+    }
+

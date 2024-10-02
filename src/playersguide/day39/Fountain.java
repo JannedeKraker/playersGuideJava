c7229903de9e73e40fc99550ab95dd10222e7c50
ennaj_janne <ennaj_janne@hotmail.com>
2024-10-02 12:20:53 +0200

    Day 39 , OOP

---
+++ src/playersguide/day39/Fountain.java
@@ -0,0 +1,13 @@
+package playersguide.day39;
+
+public class Fountain {
+    private boolean fountainState;
+
+    public void setFountainState(boolean fountainOnOrOff){
+        this.fountainState = fountainOnOrOff;
+    }
+    public boolean getFountainState(){
+        return fountainState;
+    }
+
+}
--- src/playersguide/day39/GridOfRooms.java
+++ src/playersguide/day39/GridOfRooms.java
@@ -2,16 +2,30 @@ package playersguide.day39;
 
 public class GridOfRooms {
 
-  private  final String[][] gridOfRooms = {{"entrance", null, "fountain", null},
+    private final String[][] gridOfRooms = {{"entrance", null, "fountain", null},
             {null, null, null, null},
             {null, null, null, null},
             {null, null, null, null}};
 
+    private Fountain fountain = new Fountain();
 
-  private final int startRoomRow = 0;
-  private final int startRoomColumn = 0;
-  private int whichRoomAreWeRow;
-  private int whichRoomAreWeColumn;
+    private final int startRoomRow = 0;
+    private final int startRoomColumn = 0;
+    private int whichRoomAreWeRow;
+    private int whichRoomAreWeColumn;
+
+    public String doSomethingAtTheRoom(String command) {
+        switch (command) {
+            case "exit":
+                return "You feel the sun is shining, your eyes have to get used to the bright light";
+            case "fountain on":
+                fountain.setFountainState(true);
+                return "You hear water falling. you feel splashes on your cheek.";
+            case "fountain off":
+                fountain.setFountainState(false);
+                return "You hear a dripping sound.";
+        }
+    }
 
     public void movingInGrid(String move) {
         switch (move) {
@@ -34,12 +48,34 @@ public class GridOfRooms {
                 break;
         }
     }
-    String getRoomCoordinates(){
-       return "["+ whichRoomAreWeRow + "]" + "["+ whichRoomAreWeColumn + "]";
+
+    public String getRoomCoordinates() {
+        return "[" + whichRoomAreWeRow + "]" + "[" + whichRoomAreWeColumn + "]";
     } // Wat is beter? met String naam of zonder?
-    String getRoomContents() {
+
+    public String getRoomContents() {
         String roomContent = gridOfRooms[whichRoomAreWeRow][whichRoomAreWeColumn];
         return roomContent;
     } // Wat is beter? met String naam of zonder?
+
+    public String experienceRoom() {
+        switch (getRoomContents()) {
+            case "null":
+                return "It is very quiet and dark here.";
+            case "entrance":
+                return "light shines in from outside. You are at the entrance.";
+            case "fountain":
+                if (fountain.getFountainState()) {
+                    return "You hear water falling. you feel splashes on your cheek.";
+                } else {
+                    return "you hear a dripping sound";
+                }
+            default:
+                return "we are lost";
+
+        }
+
+
+    }
 
 }
